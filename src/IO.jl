fff7d8db19be190023f5507712c9703adc359056
Manuel F. Schmid <mfs2173@columbia.edu>
2024-02-09 01:03:07 -0500

    Implement parsing of VLR data

--- src/IO.jl
+++ src/IO.jl
@@ -38,12 +38,72 @@ function Base.read(io::Base.IO, ::Type{GUID})
   GUID(p1, p2, p3, p4)
 end
 
+struct VariableLengthRecord
+  user_id::String
+  record_id::UInt16
+  data::Vector{UInt8}
+  description::String
+end
+
+function Base.show(io::Base.IO, vlr::VariableLengthRecord)
+  print(io, vlr.user_id, "[", vlr.record_id, "]")
+  if !isempty(vlr.description)
+    print(io, " \"", vlr.description, "\"")
+  end
+  print(io, " (", length(vlr.data), " bytes)")
+end
+
+function Base.read(io::Base.IO, ::Type{VariableLengthRecord}; max_bytes = nothing, version = nothing)
+
+  # check whether there are enough bytes left to read the record header
+  if !isnothing(max_bytes) && max_bytes < 54
+    return # data will end up in user-defined bytes
+  end
+
+  # read header fields
+  reserved = read(io, UInt16)
+  user_id = bytes_to_string(read!(io, Vector{UInt8}(undef, 16)))
+  record_id = read(io, UInt16)
+  record_length = read(io, UInt16)
+  description = bytes_to_string(read!(io, Vector{UInt8}(undef, 32)))
+
+  # check whether there are enough bytes left to read the record data
+  if !isnothing(max_bytes) && max_bytes < 54 + record_length
+    @error "Invalid variable-length records"
+    record_length = max_bytes - 54
+    # still build the truncated record to be conservative
+  end
+
+  # check value of reserved field
+  if isnothing(version)
+    # skip checks
+  elseif version >= (1, 4)
+    if !iszero(reserved)
+      rstr = uppercase(string(reserved, base = 16, pad = 4))
+      @warn "Reserved field in VLR header has non-zero value 0x$rstr"
+    end
+  elseif version == (1, 0)
+    if reserved != 0xAABB
+      rstr = uppercase(string(reserved, base = 16, pad = 4))
+      @warn "Reserved field in VLR header has non-standard value 0x$rstr"
+    end
+  elseif !(iszero(reserved) || reserved == 0xAABB)
+    # standards 1.1–1.3 do not specify the reserved field, but having a
+    # value other than those two is unexpected enough to warrant a warning
+    rstr = uppercase(string(reserved, base = 16, pad = 4))
+    @warn "Reserved field in VLR header has non-standard value 0x$rstr"
+  end
+
+  data = read(io, record_length)
+  VariableLengthRecord(user_id, record_id, data, description)
+end
+
 mutable struct LAS{T,P,V}
 
   # point & VLR data can be in custom containers
   points::P
   vlrs::V
-  user_defined_bytes::Vector{UInt8}
+  extra_data::Vector{UInt8}
 
   # rescaling of coordinate numbers
   coord_scale::NTuple{3,Float64}
@@ -89,12 +149,30 @@ function Base.show(io::Base.IO, las::LAS)
     end
   end
   print(io, ", v$(las.version[1]).$(las.version[2]))")
+
   pad = 14
   print(io, rpad("\n  Source ID", pad), "=> ", iszero(las.source_id) ? "(unassigned)" : string(las.source_id))
   print(io, rpad("\n  Project ID", pad), "=> ")
   show(io, las.project_id)
   print(io, rpad("\n  System ID", pad), "=> \"", las.system_id, "\"")
   print(io, rpad("\n  Software ID", pad), "=> \"", las.software_id, "\"")
+
+  nextra = length(las.extra_data)
+  if !iszero(nextra)
+    limit = nextra > 8 ? 6 : nextra
+    print(io, rpad("\n  Extra Data", pad), "=> ")
+    print(io, "[", join(("0x" * uppercase(string(b, base = 16, pad = 2)) for b in las.extra_data[1:limit]), ", "))
+    nextra > limit && print(io, " … (", nextra - limit, " more bytes)")
+    print(io, "]")
+  end
+
+  nvlr = length(las.vlrs)
+  print(io, "\n  Variable-Length Records")
+  map(las.vlrs[1:min(end, 5)]) do vlr
+    print(io, "\n    => ")
+    show(io, vlr)
+  end
+  nvlr > 5 && println("\n    => ($(nvlr - 5) more records)")
 end
 
 function read_las_signature(io)
@@ -245,17 +323,24 @@ function Base.read(io::Base.IO, ::Type{LAS})
   # read VLR data only up to the point-data offset so we can still attempt to
   # read the point data if there is an issue with the VLR data
   remaining = point_data_offset - bytes_read
-  user_defined_bytes = read(io, remaining)
+  vlrs = VariableLengthRecord[]
+  for _ in 1:vlr_count
+    iszero(remaining) && break
+    vlr = read(io, VariableLengthRecord; version = version, max_bytes = remaining)
+    isnothing(vlr) && break
+    push!(vlrs, vlr)
+    remaining -= 54 + length(vlr.data)
+  end
+  extra_data = read(io, remaining)
 
   # for now, skip loading actual data
   pttype = (pdrf_type, pdrf_length)
   points = point_count_total
-  vlrs = vlr_count
 
   LAS{pttype, typeof(points), typeof(vlrs)}(
     points,
     vlrs,
-    user_defined_bytes,
+    extra_data,
     # rescaling of coordinate numbers
     coord_scale,
     coord_offset,
