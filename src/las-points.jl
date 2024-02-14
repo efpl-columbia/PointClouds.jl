5e9b5720f457e58fc1e2a7f3da10272da90ff808
Manuel F. Schmid <mfs2173@columbia.edu>
2024-02-14 12:29:18 -0500

    Add accessor functions for point properties

--- src/las-points.jl
+++ src/las-points.jl
@@ -1,14 +1,17 @@
 abstract type PointRecord{F,N} end
+abstract type LegacyPointRecord{F,N} <: PointRecord{F,N} end
+
+const WaveformPacket = Tuple{UInt8,UInt64,UInt32,Float32,Float32,Float32,Float32}
 
 struct UnknownPointRecord{F,N} <: PointRecord{F,N}
   data::NTuple{N,UInt8}
 end
 
-struct PointRecord0{N} <: PointRecord{0,N}
+struct PointRecord0{N} <: LegacyPointRecord{0,N}
   # core items (legacy formats)
   coords::NTuple{3,Int32}
   intensity::UInt16
-  category::NTuple{2,UInt8}
+  attributes::NTuple{2,UInt8}
   scan_angle::Int8
   user_data::UInt8
   source_id::UInt16
@@ -16,11 +19,11 @@ struct PointRecord0{N} <: PointRecord{0,N}
   extra_bytes::NTuple{N,UInt8}
 end
 
-struct PointRecord1{N} <: PointRecord{1,N}
+struct PointRecord1{N} <: LegacyPointRecord{1,N}
   # core items (legacy formats)
   coords::NTuple{3,Int32}
   intensity::UInt16
-  category::NTuple{2,UInt8}
+  attributes::NTuple{2,UInt8}
   scan_angle::Int8
   user_data::UInt8
   source_id::UInt16
@@ -30,11 +33,11 @@ struct PointRecord1{N} <: PointRecord{1,N}
   extra_bytes::NTuple{N,UInt8}
 end
 
-struct PointRecord2{N} <: PointRecord{2,N}
+struct PointRecord2{N} <: LegacyPointRecord{2,N}
   # core items (legacy formats)
   coords::NTuple{3,Int32}
   intensity::UInt16
-  category::NTuple{2,UInt8}
+  attributes::NTuple{2,UInt8}
   scan_angle::Int8
   user_data::UInt8
   source_id::UInt16
@@ -44,11 +47,11 @@ struct PointRecord2{N} <: PointRecord{2,N}
   extra_bytes::NTuple{N,UInt8}
 end
 
-struct PointRecord3{N} <: PointRecord{3,N}
+struct PointRecord3{N} <: LegacyPointRecord{3,N}
   # core items (legacy formats)
   coords::NTuple{3,Int32}
   intensity::UInt16
-  category::NTuple{2,UInt8}
+  attributes::NTuple{2,UInt8}
   scan_angle::Int8
   user_data::UInt8
   source_id::UInt16
@@ -59,41 +62,33 @@ struct PointRecord3{N} <: PointRecord{3,N}
   extra_bytes::NTuple{N,UInt8}
 end
 
-struct PointRecord4{N} <: PointRecord{4,N}
+struct PointRecord4{N} <: LegacyPointRecord{4,N}
   # core items (legacy formats)
   coords::NTuple{3,Int32}
   intensity::UInt16
-  category::NTuple{2,UInt8}
+  attributes::NTuple{2,UInt8}
   scan_angle::Int8
   user_data::UInt8
   source_id::UInt16
   # format 4: gps time & waveform packets
   gps_time::Float64
-  waveform_descriptor::UInt8
-  waveform_offset::UInt64
-  waveform_length::UInt32
-  waveform_location::Float32
-  waveform_direction::NTuple{3,Float32}
+  waveform_packet::WaveformPacket
   # optional extra data
   extra_bytes::NTuple{N,UInt8}
 end
 
-struct PointRecord5{N} <: PointRecord{5,N}
+struct PointRecord5{N} <: LegacyPointRecord{5,N}
   # core items (legacy formats)
   coords::NTuple{3,Int32}
   intensity::UInt16
-  category::NTuple{2,UInt8}
+  attributes::NTuple{2,UInt8}
   scan_angle::Int8
   user_data::UInt8
   source_id::UInt16
   # format 5: gps time, rgb color & waveform packets
   gps_time::Float64
   color_channels::NTuple{3,UInt16}
-  waveform_descriptor::UInt8
-  waveform_offset::UInt64
-  waveform_length::UInt32
-  waveform_location::Float32
-  waveform_direction::NTuple{3,Float32}
+  waveform_packet::WaveformPacket
   # optional extra data
   extra_bytes::NTuple{N,UInt8}
 end
@@ -102,7 +97,7 @@ struct PointRecord6{N} <: PointRecord{6,N}
   # core items (new formats)
   coords::NTuple{3,Int32}
   intensity::UInt16
-  category::NTuple{3,UInt8}
+  attributes::NTuple{3,UInt8}
   user_data::UInt8
   scan_angle::Int16
   source_id::UInt16
@@ -115,7 +110,7 @@ struct PointRecord7{N} <: PointRecord{7,N}
   # core items (new formats)
   coords::NTuple{3,Int32}
   intensity::UInt16
-  category::NTuple{3,UInt8}
+  attributes::NTuple{3,UInt8}
   user_data::UInt8
   scan_angle::Int16
   source_id::UInt16
@@ -130,7 +125,7 @@ struct PointRecord8{N} <: PointRecord{8,N}
   # core items (new formats)
   coords::NTuple{3,Int32}
   intensity::UInt16
-  category::NTuple{3,UInt8}
+  attributes::NTuple{3,UInt8}
   user_data::UInt8
   scan_angle::Int16
   source_id::UInt16
@@ -145,17 +140,13 @@ struct PointRecord9{N} <: PointRecord{9,N}
   # core items (new formats)
   coords::NTuple{3,Int32}
   intensity::UInt16
-  category::NTuple{3,UInt8}
+  attributes::NTuple{3,UInt8}
   user_data::UInt8
   scan_angle::Int16
   source_id::UInt16
   gps_time::Float64
   # format 9: waveform packets
-  waveform_descriptor::UInt8
-  waveform_offset::UInt64
-  waveform_length::UInt32
-  waveform_location::Float32
-  waveform_direction::NTuple{3,Float32}
+  waveform_packet::WaveformPacket
   # optional extra data
   extra_bytes::NTuple{N,UInt8}
 end
@@ -164,20 +155,67 @@ struct PointRecord10{N} <: PointRecord{10,N}
   # core items (new formats)
   coords::NTuple{3,Int32}
   intensity::UInt16
-  category::NTuple{3,UInt8}
+  attributes::NTuple{3,UInt8}
   user_data::UInt8
   scan_angle::Int16
   source_id::UInt16
   gps_time::Float64
   # format 10: rgb + nir color & waveform packets
   color_channels::NTuple{4,UInt16}
-  waveform_descriptor::UInt8
-  waveform_offset::UInt64
-  waveform_length::UInt32
-  waveform_location::Float32
-  waveform_direction::NTuple{3,Float32}
+  waveform_packet::WaveformPacket
   # optional extra data
   extra_bytes::NTuple{N,UInt8}
+end
+
+# simple accessors for fields that always exist
+# note: intensity is optional, but we still return 0 and not missing for type stability
+integer_coordinates(pt) = pt.coords
+intensity(pt) = pt.intensity
+encoded_attributes(pt) = pt.attributes
+integer_scan_angle(pt) = pt.scan_angle
+user_data(pt) = pt.user_data
+source_id(pt) = pt.source_id
+extra_bytes(pt) = pt.extra_bytes
+
+# return missing for formats that do not have a field
+gps_time(pt) = hasfield(typeof(pt), :gps_time) ? pt.gps_time : missing
+color_channels(pt) = hasfield(typeof(pt), :color_channels) ? pt.color_channels : missing
+waveform_packet(pt) = hasfield(typeof(pt), :waveform_packet) ? pt.waveform_packet : missing
+
+# decode attribute bits for legacy formats
+return_number(pt::LegacyPointRecord) = pt.attributes[1] & 0b00000111 # bits 0–2
+return_count(pt::LegacyPointRecord) = (pt.attributes[1] & 0b00111000) >> 3 # bits 3–5
+is_left_to_right(pt::LegacyPointRecord) = !iszero(pt.attributes[1] & 0b01000000) # bit 6
+is_right_to_left(pt::LegacyPointRecord) = !is_left_to_right(pt)
+is_edge_of_line(pt::LegacyPointRecord) = !iszero(pt.attributes[1] & 0b10000000) # bit 7
+classification(pt::LegacyPointRecord) = pt.attributes[2] & 0b00011111 # bits 0–4
+is_synthetic(pt::LegacyPointRecord) = !iszero(pt.attributes[2] & 0b00100000) # bit 5
+is_key_point(pt::LegacyPointRecord) = !iszero(pt.attributes[2] & 0b01000000) # bit 6
+is_withheld(pt::LegacyPointRecord) = !iszero(pt.attributes[2] & 0b10000000) # bit 7
+is_overlap(pt::LegacyPointRecord) = classification(pt) == 12
+
+# decode attribute bits for newer formats
+# TODO: decide whether classification(pt) == 12 should also be treated as overlap
+return_number(pt::PointRecord) = pt.attributes[1] & 0b00001111 # bits 0–3
+return_count(pt::PointRecord) = (pt.attributes[1] & 0b11110000) >> 4 # bits 4–7
+is_synthetic(pt::PointRecord) = !iszero(pt.attributes[2] & 0b00000001) # bit 0
+is_key_point(pt::PointRecord) = !iszero(pt.attributes[2] & 0b00000010) # bit 1
+is_withheld(pt::PointRecord) = !iszero(pt.attributes[2] & 0b00000100) # bit 2
+is_overlap(pt::PointRecord) = !iszero(pt.attributes[2] & 0b00001000) # bit 3
+scanner_channel(pt::PointRecord) = (pt.attributes[2] & 0b00110000) >> 4 # bits 4–5
+is_left_to_right(pt::PointRecord) = !iszero(pt.attributes[2] & 0b01000000) # bit 6
+is_right_to_left(pt::PointRecord) = !is_left_to_right(pt)
+is_edge_of_line(pt::PointRecord) = !iszero(pt.attributes[2] & 0b10000000) # bit 7
+classification(pt::PointRecord) = pt.attributes[3] # bits 0–7
+
+# provide access to rescaled scan angle
+function scan_angle(pt::LegacyPointRecord)
+  -90 <= pt.scan_angle <= 90 || @error "Scan angle outside the valid range of −90° to +90°"
+  pt.scan_angle * 1.0
+end
+function scan_angle(pt::PointRecord)
+  -30_000 <= pt.scan_angle <= 30_000 || @error "Scan angle outside the valid range of −180° to +180°"
+  pt.scan_angle * 0.006
 end
 
 function Base.show(io::Base.IO, ::Type{<:PointRecord{F,N}}) where {F,N}
@@ -337,11 +375,4 @@ function point_record_type(pdrf, bytes)
     @error "Unknown Point Data Record Format: $pdrf"
     UnknownPointRecord{Int(pdrf),Int(bytes)}
   end
-end
-
-function return_number(pt::PointRecord{F}) where {F}
-  if F <= 5 # legacy point formats
-    return Int(pt.category[1] & 0b00000111) # bits 0–2
-  end
-  pt.category[1] & 0b00001111 # bits 0–3
 end
