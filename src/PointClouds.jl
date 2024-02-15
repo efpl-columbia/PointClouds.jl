0a83834089c50b97c7d264dd3c80f211ba6ddbc0
Manuel F. Schmid <mfs2173@columbia.edu>
2024-02-15 14:26:51 -0500

    Export accessors for point fields

--- src/PointClouds.jl
+++ src/PointClouds.jl
@@ -2,6 +2,9 @@ module PointClouds
 
 export LAS
 
+# accessors for point fields
+export classification, color_channels, encoded_attributes, extra_bytes, gps_time, integer_coordinates, integer_scan_angle, intensity, is_edge_of_line, is_key_point, is_left_to_right, is_overlap, is_right_to_left, is_synthetic, is_withheld, return_count, return_number, scan_angle, scanner_channel, source_id, user_data, waveform_packet
+
 include("IO.jl")
 
 using .IO
